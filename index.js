756bd3f83fa98f879ec0919b497064451b4573e0
SantinoHamada <hamadasantino@hotmail.com>
2024-09-23 11:20:54 -0300

    agregamos el puerto 400 o una variable de entorno en nuestra configuracion principal

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hola mundo")
+import express from "express"
+
+//1- configurar un puerto
+
+const app = express()
+
+app.set("port",process.env.PORT||4000)
+app.listen(app.get("port"),()=>{
+    console.info('Estoy escuchando el puerto '+app.get("port"))
+})
+//2- configurar middlewares
+
+//3- configurar las rutas
