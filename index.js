bdbf81c8ec87e853882e74694c6e0735d20957d4
SantinoHamada <hamadasantino@hotmail.com>
2024-09-25 09:48:09 -0300

    agregamos los middlewares cors, morgan y los de express

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import express from "express"
-
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 //1- configurar un puerto
 
-const app = express()
+const app = express();
 
-app.set("port",process.env.PORT||4000)
-app.listen(app.get("port"),()=>{
-    console.info('Estoy escuchando el puerto '+app.get("port"))
-})
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.info("Estoy escuchando el puerto " + app.get("port"));
+});
+
 //2- configurar middlewares
 
+app.use(cors()); // permite conexiones remotas
+app.use(morgan("dev")); // nos da info extra en la terminal
+app.use(express.json()); // interpreta los datos del body en formato json
+app.use(express.urlencoded({ extended: true })); //interpreta datos enviados en formularios
+
 //3- configurar las rutas
+
+app.get("/prueba", (req, res, next) => {
+  //   console.log("desde la funcion de prueba")
+  res.send("desde el backend");
+});
